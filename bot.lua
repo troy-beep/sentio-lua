1907096edfa5df58e550c883c54332357b5a9c5c
troy-beep <krish.disbound@gmail.com>
2024-12-06 21:27:56 +0530

    Create bot.lua

---
+++ bot.lua
@@ -0,0 +1,8 @@
+Handlers.add(
+  "HandleAnnouncements",
+  { Action = "Announcement" },
+  function (msg)
+    ao.send({Target = Game, Action = "GetGameState"})
+    print(msg.Event .. ": " .. msg.Data)
+  end
+)
