cb31de203a1c9afceb383640d06162199be6f28f
troy-beep <krish.disbound@gmail.com>
2024-12-06 21:24:01 +0530

    tokengating

--- abc.lua
+++ abc.lua
@@ -17,3 +17,26 @@ end
 function isValidVendor(name)
     return name:match("^@%w+$")
 end
+
+-- token gating
+
+Handlers.add(
+    "Broadcast",
+    { Action = "Broadcast" },
+    function(m)
+        if Balances[m.From] == nil or tonumber(Balances[m.From]) < 1 then
+            print("UNAUTH REQ: " .. m.From)
+            return
+        end
+        local type = m.Type or "Normal"
+        print("Broadcasting message from " .. m.From .. ". Content: " .. m.Data)
+        for i = 1, #Members, 1 do
+            ao.send({
+                Target = Members[i],
+                Action = "Broadcasted",
+                Broadcaster = m.From,
+                Data = m.Data
+            })
+        end
+    end
+)
