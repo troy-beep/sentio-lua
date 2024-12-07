7da1d829192b1176634f9555e2fd8768fb782a6f
troy-beep <krish.disbound@gmail.com>
2024-12-07 15:05:42 +0530

    Update chatroom.lua

--- chatroom.lua
+++ chatroom.lua
@@ -1,7 +1,4 @@
 
--- Modify `chatroom.lua` to include a handler for `Members`
--- to register to the chatroom with the following code:
-
   Handlers.add(
     "Register",
     { Action = "Register"},
